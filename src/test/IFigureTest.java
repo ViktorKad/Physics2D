8631d3c929ca88a82acb89ba5db1b7505f37169c
Kadegrov Viktor <kad-git@yandex.ru>
2015-02-02 13:34:39 +0300

    Обновлены тесты

--- src/test/IFigureTest.java
+++ src/test/IFigureTest.java
@@ -22,16 +22,19 @@ public class IFigureTest {
         circle1 = FigureFactory.getCircle(2, 2, 1);
         circle2 = FigureFactory.getCircle(4, 2, 1);
         assertTrue(circle1.checkCollision(circle2));
+        assertTrue(circle2.checkCollision(circle1));
 
         // Окружность вписанная в окружность
         circle1 = FigureFactory.getCircle(5, 5, 3);
         circle2 = FigureFactory.getCircle(6, 6, 1);
         assertTrue(circle1.checkCollision(circle2));
+        assertTrue(circle2.checkCollision(circle1));
 
         // Отсутствие пересечения
         circle1 = FigureFactory.getCircle(2, 2, 0.9f);
         circle2 = FigureFactory.getCircle(4, 2, 1);
         assertFalse(circle1.checkCollision(circle2));
+        assertFalse(circle2.checkCollision(circle1));
 
 
         System.out.println(IFigure.class + ",  method check(Rectangle, Rectangle)");
@@ -39,16 +42,19 @@ public class IFigureTest {
         polygon1 = FigureFactory.getRectangle(0, 0, 2, 2);
         polygon2 = FigureFactory.getRectangle(1, 1, 2, 2);
         assertTrue(polygon1.checkCollision(polygon2));
+        assertTrue(polygon2.checkCollision(polygon1));
 
         // Пересечение по одной стороне
         polygon1 = FigureFactory.getRectangle(0, 0, 2, 2);
         polygon2 = FigureFactory.getRectangle(-1, -1, 2, 2);
         assertTrue(polygon1.checkCollision(polygon2));
+        assertTrue(polygon2.checkCollision(polygon1));
 
         // Отсутствие пересечения
         polygon1 = FigureFactory.getRectangle(1, 1, 10, 1);
         polygon2 = FigureFactory.getRectangle(1, 3, 10, 1);
         assertFalse(polygon1.checkCollision(polygon2));
+        assertFalse(polygon2.checkCollision(polygon1));
 
 
         System.out.println(IFigure.class + ",  method check(Triangle, Triangle)");
@@ -56,11 +62,23 @@ public class IFigureTest {
         polygon1 = FigureFactory.getTriangle(1,1, 1,4, 4,1);
         polygon2 = FigureFactory.getTriangle(2,2, 2,5, 5,2);
         assertTrue(polygon1.checkCollision(polygon2));
+        assertTrue(polygon2.checkCollision(polygon1));
 
         // Отсутствие пересечения треугольников
         polygon1 = FigureFactory.getTriangle(1,1, 1,4, 4,1);
         polygon2 = FigureFactory.getTriangle(3,3, 3,6, 6,3);
         assertFalse(polygon1.checkCollision(polygon2));
+        assertFalse(polygon2.checkCollision(polygon1));
+
+
+
+
+        System.out.println(IFigure.class + ",  method check(Rectangle, Triangle)");
+        // Пересечение треугольника и прямоугольника
+        polygon1 = FigureFactory.getTriangle(5,5, 5,10, 10,5);
+        polygon2 = FigureFactory.getRectangle(7, 7, 8, 2);
+        assertTrue(polygon1.checkCollision(polygon2));
+//        assertTrue(polygon2.checkCollision(polygon1));
 
 
         // TODO: вернуть, когда будет реализована проверка пересечения полигона с кругом
@@ -69,15 +87,18 @@ public class IFigureTest {
 //        circle1 = FigureFactory.getCircle(2, 2, 1);
 //        rect1 = FigureFactory.getRectangle(0, 0, 1.7f, 1.7f);
 //        assertTrue(circle1.checkCollision(rect1));
+//        assertTrue(rect1.checkCollision(circle1));
 //
 //        // Отсутствие пересечения
 //        circle1 = FigureFactory.getCircle(2, 2, 1);
 //        rect1 = FigureFactory.getRectangle(0, 0, 1, 1);
 //        assertFalse(circle1.checkCollision(rect1));
+//        assertFalse(rect1.checkCollision(circle1));
 //
 //        // Окружность вписанная в прямоугольник
 //        circle1 = FigureFactory.getCircle(5, 5, 2);
 //        rect1 = FigureFactory.getRectangle(2, 2, 6, 6);
 //        assertTrue(circle1.checkCollision(rect1));
+//        assertTrue(rect1.checkCollision(circle1));
     }
 }
